f63ce9914e71116f02a00e8b12869446c4baf7ce
Sander Ruiter <ruiter.sander@gmail.com>
2024-07-17 11:04:30 +0200

    Update sendlogFGT.sh

--- logtool/sendlogFGT.sh
+++ logtool/sendlogFGT.sh
@@ -7,13 +7,13 @@ fazip=172.16.1.2
 # fgtsn=FGVM01TM23001638
 speed=20
 
-/fortipoc/logtool/logtool64int send -ip $fazip -dtime-now -input /fortipoc/logtool/FGVM010000097597.elog.txt.log -devid $fgtsn -speed $speed -nmsgs -1 &
-/fortipoc/logtool/logtool64int send -ip $fazip -dtime-now -input /fortipoc/logtool/FGVM010000097597.tlog.172.18.0.0.log -devid $fgtsn -speed $speed -nmsgs -1 &
-/fortipoc/logtool/logtool64int send -ip $fazip -dtime-now -input /fortipoc/logtool/FGVM010000097597.tlog.10.200.0.0.log -devid $fgtsn -speed $speed -nmsgs -1 &
-/fortipoc/logtool/logtool64int send -ip $fazip -dtime-now -input /fortipoc/logtool/FGVM010000097597.tlog.10.100.0.0.log -devid $fgtsn -speed $speed -nmsgs -1 &
-/fortipoc/logtool/logtool64int send -ip $fazip -dtime-now -input /fortipoc/logtool/IOC.tlog.txt.log -devid $fgtsn -speed $speed -nmsgs 0 &
-/fortipoc/logtool/logtool64int send -ip $fazip -dtime-now -input /fortipoc/logtool/fct_vuln_elog.txt.log -devid $fgtsn -speed $speed -nmsgs 0 &
+/faz/fazworkshop/logtool/logtool64int send -ip $fazip -dtime-now -input /faz/fazworkshop/logtool/FGVM010000097597.elog.txt.log -devid $fgtsn -speed $speed -nmsgs -1 &
+/faz/fazworkshop/logtool/logtool64int send -ip $fazip -dtime-now -input /faz/fazworkshop/logtool/FGVM010000097597.tlog.172.18.0.0.log -devid $fgtsn -speed $speed -nmsgs -1 &
+/faz/fazworkshop/logtool/logtool64int send -ip $fazip -dtime-now -input /faz/fazworkshop/logtool/FGVM010000097597.tlog.10.200.0.0.log -devid $fgtsn -speed $speed -nmsgs -1 &
+/faz/fazworkshop/logtool/logtool64int send -ip $fazip -dtime-now -input /faz/fazworkshop/logtool/FGVM010000097597.tlog.10.100.0.0.log -devid $fgtsn -speed $speed -nmsgs -1 &
+/faz/fazworkshop/logtool/logtool64int send -ip $fazip -dtime-now -input /faz/fazworkshop/logtool/IOC.tlog.txt.log -devid $fgtsn -speed $speed -nmsgs 0 &
+/faz/fazworkshop/logtool/logtool64int send -ip $fazip -dtime-now -input /faz/fazworkshop/logtool/fct_vuln_elog.txt.log -devid $fgtsn -speed $speed -nmsgs 0 &
 #/fortipoc/logtool/logtool64int send -ip $fazip -dtime-now -input /fortipoc/logtool/BrutteForcetoSMTP.csv.log -devid $fgtsn -speed $speed -nmsgs -1 &
-/fortipoc/logtool/logtool64int send -ip $fazip -dtime-now -input /fortipoc/logtool/FGVM010000166273.tlog.log -devid $fgtsn -speed $speed -nmsgs -1 &
-/fortipoc/logtool/logtool64int send -ip $fazip -dtime-now -input /fortipoc/logtool/FGVM010000166273.elog.log -devid $fgtsn -speed $speed -nmsgs -1 &
+/faz/fazworkshop/logtool/logtool64int send -ip $fazip -dtime-now -input /faz/fazworkshop/logtool/FGVM010000166273.tlog.log -devid $fgtsn -speed $speed -nmsgs -1 &
+/faz/fazworkshop/logtool/logtool64int send -ip $fazip -dtime-now -input /faz/fazworkshop/logtool/FGVM010000166273.elog.log -devid $fgtsn -speed $speed -nmsgs -1 &
 
